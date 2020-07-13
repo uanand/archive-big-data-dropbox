eddcf5ed745293a08904d57d6a4d0ef7c3692590
Utkarsh Anand <utkarsh.aanand@gmail.com>
2020-07-13 13:13:39 +0800

    Fix logic in dropboxFree() by setting exception if check process fails. By default it is assumed that Dropbox is busy.

--- dropbox.py
+++ dropbox.py
@@ -2,25 +2,37 @@ import os
 import numpy
 import pandas
 import platform
-from tqdm import tqdm
-
+import psutil
+import shutil
 import utils
+import time
 
 class dropbox:
     
     ############################################################
-    def __init__(self,excelName,sheetName,batchSize_GB=400,sleepTime=120):
+    def __init__(self,excelName,sheetName,batchSize_GB=400,sleepTime=120,r_wSpeedCutOff=0.5):
         self.excelName = excelName
         self.sheetName = sheetName
         self.names = ['inputFile','outputDir']
         self.batchSize = batchSize_GB*1024*1024*1024
         self.sleepTime = sleepTime
+        self.r_wSpeedCutOff = r_wSpeedCutOff
         self.df = pandas.read_excel(self.excelName,sheet_name=self.sheetName,names=self.names)
+        
+        print ('Stage 2 - Data upload')
+        self.getFileList()
+        utils.mkdirs(self.dropboxDirList)
+        self.makeBatches()
+        # self.uploadFiles()
+        
+        print(self.storageFree())
+        print(self.dropboxRunning())
+        print(self.droboxFree())
     ############################################################
     
     ############################################################
-    def getAllFiles(self):
-        fileNameList,fileSizeList,dropBoxFileList,dropBoxDirList = [],[],[],[]
+    def getFileList(self):
+        fileNameList,fileSizeList,dropboxFileList,dropboxDirList = [],[],[],[]
         for inputFile,outputDir in self.df.values:
             if (os.path.isfile(inputFile)):
                 fileSize = os.path.getsize(inputFile)
@@ -48,9 +60,9 @@ class dropbox:
         for root,dirs,files in os.walk(inputDir):
             for name in files:
                 fileName = os.path.join(root,name)
-                fileSize = getsize(fileName)
-                dropboxFile = inputFile.replace(inputDir,outputDir)
-                dropboxDir = utils.getDirName(inputFile,name)
+                fileSize = os.path.getsize(fileName)
+                dropboxFile = fileName.replace(inputDir,outputDir)
+                dropboxDir = utils.getDirName(dropboxFile,name)
                 fileNameList.append(fileName)
                 fileSizeList.append(fileSize)
                 dropboxFileList.append(dropboxFile)
@@ -58,50 +70,110 @@ class dropbox:
         return fileNameList,fileSizeList,dropboxFileList,dropboxDirList
     ############################################################
     
-    # ############################################################
-    # def getFileList(self,dropboxDirFlag=True):
-        # fullFileNameList,fileSizeList,dropBoxDirList, = [],[]
-        # for inputFile,dropboxDir in self.df.values:
-            # if (os.path.isfile(inputFile)):
-                # fileSize = os.path.getsize(inputFile)
-                # fullFileNameList.append(inputFile)
-                # fileSizeList.append(fileSize)
-                # if (dropboxDirFlag==True):
-                    # if (platform.system()=='Linux'):
-                        # fileName = inputFile.split('/')[-1]
-                    # elif (platform.system()=='Windows'):
-                        # fileName = inputFile.split('\\')[-1]
-                    # dropBoxDirList.append(dropboxDir)
-                    # dropboxFileList.append(dropboxDir+'/'+fileName)
-            # elif (os.path.isdir(inputFile)):
-                # inputDir = inputFile
-                # tempFileNameList,tempFileSizeList, = self.getFilesinDir(inputDir,dropboxDir,dropboxDirFlag)
-                # for a,b in zip(tempFileNameList,tempFileSizeList):
-                    # fullFileNameList.append(a)
-                    # fileSizeList.append(b)
-        # self.fullFileNameList = fullFileNameList
-        # self.fileSizeList = fileSizeList
-    # ############################################################
+    ############################################################
+    def makeBatches(self):
+        fileNameBatch,dropboxFileBatch = [],[]
+        l1,l2,totalSize = [],[],0
+        lastFileIncluded = False
+        for fileName,fileSize,dropboxFile in zip(self.fileNameList,self.fileSizeList,self.dropboxFileList):
+            totalSize += fileSize
+            if (totalSize <= self.batchSize):
+                l1.append(fileName)
+                l2.append(dropboxFile)
+            elif (totalSize > self.batchSize):
+                fileNameBatch.append(l1)
+                dropboxFileBatch.append(l2)
+                l1,l2,totalSize = [],[],0
+                l1.append(fileName)
+                l2.append(dropboxFile)
+                totalSize += fileSize
+            if (fileName == self.fileNameList[-1]):
+                fileNameBatch.append(l1)
+                dropboxFileBatch.append(l2)
+        self.fileNameBatch = fileNameBatch
+        self.dropboxFileBatch = dropboxFileBatch
+        self.numBatches = len(fileNameBatch)
+    ############################################################
     
-    # ############################################################
-    # def getFilesinDir(self,inputDir,outputDir=None):
-        # if (outputDir==None):
-            # fileNameList,fileSizeList = [],[]
-            # for root,dirs,files in os.walk(inputDir):
-                # for name in files:
-                    # fileName = os.path.join(root,name)
-                    # fileSize = getsize(fileName)
-                    # fileNameList.append(fileName)
-                    # fileSizeList.append(fileSize)
-            # return fileNameList,fileSizeList
-        # else:
-            # fileNameList,fileSizeList,outputFileList,outputDirList = [],[],[],[]
-            # for root,dirs,files in os.walk(inputDir):
-                # for name in files:
-                    # fileName = os.path.join(root,name)
-                    # fileSize = getsize(fileName)
-                    # uploadFileName = fileName.replace(inputDir,)
-                    # fileNameList.append(fileName)
-                    # fileSizeList.append(fileSize)
-            # return fileNameList,fileSizeList
-    # ############################################################
+    ############################################################
+    def uploadFiles(self):
+        for i in range(self.numBatches):
+            resourceAvailable = False
+            while (resourceAvailable == False):
+                resourceAvailable = self.checkResource()
+            print ('Working on batch %d/%d' %(i+1,self.numBatches))
+            for fileName,dropboxFile in zip(self.fileNameBatch[i],self.dropboxFileBatch[i]):
+                print ('Moving %s\tto\t%s' %(fileName,dropboxFile))
+                shutil.move(fileName,dropboxFile)
+    ############################################################
+    
+    ############################################################
+    def checkResource(self):
+        resourceAvailable = False
+        if (self.droboxFree() and self.storageFree() and self.dropboxRunning()): # and self.dropboxStorageFree()
+            resourceAvailable = True
+        return resourceAvailable
+    ############################################################
+    
+    ############################################################
+    def droboxFree(self):
+        free,failed = False,False
+        read_speed,write_speed = 0,0
+        try:
+            for process in psutil.process_iter():
+                if (platform.system() == 'Windows'):
+                    processName = 'Dropbox.exe'
+                elif (platform.system() == 'Linux'):
+                    processName = 'dropbox'
+                if (process.name() == processName):
+                    read_bytes_0 = process.io_counters().read_bytes
+                    write_bytes_0 = process.io_counters().write_bytes
+                    time.sleep(self.sleepTime)
+                    read_bytes_1 = process.io_counters().read_bytes
+                    write_bytes_1 = process.io_counters().write_bytes
+                    read_speed = max(read_speed,(read_bytes_1-read_bytes_0)/1024/1024/self.sleepTime)
+                    write_speed = max(write_speed,(write_bytes_1-write_bytes_0)/1024/1024/self.sleepTime)
+        except:
+            failed = True
+        if (failed==True):
+            free = False
+        elif (read_speed < self.r_wSpeedCutOff and write_speed < self.r_wSpeedCutOff):
+            free = True
+        return free
+    ############################################################
+    
+    ############################################################
+    def storageFree(self):
+        free = False
+        availableSpace = psutil.disk_usage(self.dropboxDirList[0]).free
+        if (availableSpace >= 2*self.batchSize):
+            free = True
+        else:
+            input('Disk is full. Move data to online-only mode. Press enter to continue after more space is available.')
+        return free
+    ############################################################
+    
+    ############################################################
+    def dropboxRunning(self):
+        running = False
+        counter = 0
+        for process in psutil.process_iter():
+            if (platform.system()=='Windows'):
+                if (process.name() == 'Dropbox.exe'):
+                    counter += 1
+            elif (platform.system()=='Linux'):
+                if (process.name() == 'dropbox'):
+                    counter += 1
+        if (counter>=1):
+            running = True
+        else:
+            input('Dropbox not running. Make sure to start application. Press enter to continue after application starts.')
+        return running
+    ############################################################
+    
+    ############################################################
+    def dropboxStorageFree(self):
+        # TODO
+        pass
+    ############################################################
+    
