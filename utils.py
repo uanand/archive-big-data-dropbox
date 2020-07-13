1bce0e659e359cb2e3c1a42bcbfb785ae296153d
Utkarsh Anand <utkarsh.aanand@gmail.com>
2020-07-14 00:46:45 +0800

    Helper functions to return current time and combine log files.

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import os
 import platform
+import datetime
 
 ############################################################
 def getFileName(inputFile):
@@ -24,4 +25,30 @@ def mkdirs(dirNameList):
     for dirName in dirNameList:
         if (os.path.exists(dirName)==False):
             os.makedirs(dirName)
+############################################################
+
+############################################################
+def timestamp():
+    return datetime.datetime.now().strftime("%Y%m%d %H:%M:%S")
+############################################################
+
+############################################################
+def combineLogs():
+    logList = []
+    if os.path.exists('logs/archive.log'):
+        logList.append('logs/archive.log')
+    if os.path.exists('logs/dataPrep.log'):
+        logList.append('logs/dataPrep.log')
+    if os.path.exists('logs/dropboxApp.log'):
+        logList.append('logs/dropboxApp.log')
+    if os.path.exists('logs/dropboxAPI.log'):
+        logList.append('logs/dropboxAPI.log')
+        
+    logFile = open(datetime.datetime.now().strftime("%Y%m%d_%H%M%S")+'.log','w')
+    for log in logList:
+        f = open(log,'r')
+        logFile.write(f.read())
+        f.close()
+        os.remove(log)
+    logFile.close()
 ############################################################
