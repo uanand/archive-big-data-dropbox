5321497f1c549fe13043d685a38d85e17b90de23
Utkarsh Anand <utkarsh.aanand@gmail.com>
2020-07-14 00:45:21 +0800

    Set up the upload preferences.

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 import dataPrep
 import dropbox
+import utils
 
 excelName = '/home/utkarsh/Projects/archive-big-data-dropbox/inputs.xlsx'
 sheetName = 'dropboxUpload_APP'
+fileSizeLimit_GB = 4
+chunkSizeSplit_MB = 1024
+batchSize_GB = 10
+sleepTime = 120
+r_wSpeedCutOff = 0.2
 
-dp = dataPrep.dataPrep(excelName,sheetName,fileSizeLimit_GB=4,chunkSizeSplit_MB=1024)
-dbx = dropbox.dropboxApp(excelName,sheetName,batchSize_GB=10,sleepTime=120,r_wSpeedCutOff=0.5)
+dp = dataPrep.dataPrep(excelName,sheetName,fileSizeLimit_GB,chunkSizeSplit_MB)
+dbx = dropbox.dropboxApp(excelName,sheetName,batchSize_GB,sleepTime,r_wSpeedCutOff)
+utils.combineLogs(['logs/dataPrep.log','logs/dropboxApp.log'])
