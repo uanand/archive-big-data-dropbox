87cdc5208932040d038ccdf747372124df0b4c78
Utkarsh Anand <utkarsh.aanand@gmail.com>
2020-07-24 23:35:33 +0800

    Docstring for utils.py.

--- utils.py
+++ utils.py
@@ -4,6 +4,23 @@ import datetime
 
 ############################################################
 def getFileName(inputFile):
+    """ Get the name of file from full file path.
+    
+    Parameters:
+    ----------
+    inputFile : str
+        name of the file with file path
+        
+    Returns:
+    -------
+    fileName : str
+        name of the file
+        
+    Usage:
+    -----
+    fileName = splitFile(inputFile)
+    """
+    
     if (platform.system()=='Linux'):
         fileName = inputFile.split('/')[-1]
     elif (platform.system()=='Windows'):
@@ -13,6 +30,25 @@ def getFileName(inputFile):
 
 ############################################################
 def getDirName(inputFile,name):
+    """ Get the directory where a file is located.
+    
+    Parameters:
+    ----------
+    inputFile : str
+        name of the file with full file path.
+    name : str
+        name of the file.
+        
+    Returns:
+    -------
+    outputDir : str
+        directory where the file is located.
+        
+    Usage:
+    -----
+    dirName = getDirName(inputFile,name)
+    """
+    
     if (platform.system()=='Linux'):
         outputDir = inputFile.replace('/'+name,'')
     elif (platform.system()=='Windows'):
@@ -22,6 +58,23 @@ def getDirName(inputFile,name):
 
 ############################################################
 def mkdirs(dirNameList):
+    """ Make directories based on the liat of input directories. If the
+    directory exists, nothing is done.
+    
+    Parameters:
+    ----------
+    dirNameList : list
+        List of directories that need to be created.
+        
+    Returns:
+    -------
+    NULL
+    
+    Usage:
+    -----
+    mkdirs(dirNameList)
+    """
+    
     for dirName in dirNameList:
         if (os.path.exists(dirName)==False):
             os.makedirs(dirName)
@@ -29,11 +82,44 @@ def mkdirs(dirNameList):
 
 ############################################################
 def timestamp():
+    """ Return the current date time as a string in format
+    'YYYYMMDD HH:MM:SS'.
+    
+    Parameters:
+    ----------
+    NULL
+        
+    Returns:
+    -------
+    Current date  and time.
+    
+    Usage:
+    -----
+    dt = timestamp()
+    """
+    
     return datetime.datetime.now().strftime("%Y%m%d %H:%M:%S")
 ############################################################
 
 ############################################################
 def combineLogs():
+    """ Combine the logs of archiving, data preparation, and uploading
+    stage. The new log is saved in logs directory with the current date
+    and time 'YYYYMMDD_HHMMSS.log'.
+    
+    Parameters:
+    ----------
+    NULL
+        
+    Returns:
+    -------
+    NULL
+    
+    Usage:
+    -----
+    combineLogs()
+    """
+    
     logList = []
     if os.path.exists('logs/archive.log'):
         logList.append('logs/archive.log')
