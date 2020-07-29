44beca9f91e0e093fa4fabeba4c491a9c123ed66
Utkarsh Anand <utkarsh.aanand@gmail.com>
2020-07-30 00:51:20 +0800

    Use API to make sure the batch upload is complete.

--- app.py
+++ app.py
@@ -15,14 +15,14 @@ dropboxDir = r'E:\Dropbox'   # DIRECTORY WHERE DROPBOX SYNC DIRECTORY IS LOCATED
 batchSize_GB = 200
 sleepTime_s = 120
 r_wSpeedCutOff = 0.2
+accessToken = '*************' # GET DROPBOX ACCESS TOKEN BY CREATING AN APP HERE - https://www.dropbox.com/developers/apps
 
 # PARAMETERS FOR DATA UPLOAD USING API
-accessToken = '***************' # GET DROPBOX ACCESS TOKEN BY CREATING AN APP HERE - https://www.dropbox.com/developers/apps
 chunkSize_MB = 128 # in MB
 
 dp = dataPrep.dataPrep(excelName,sheetName,fileSizeLimit_GB,chunkSizeSplit_MB)
 if ('APP' in sheetName):
-    dbx = dropboxBatch.dropboxApp(excelName,sheetName,dropboxDir,batchSize_GB,sleepTime_s,r_wSpeedCutOff)
+    dbx = dropboxBatch.dropboxApp(excelName,sheetName,dropboxDir,batchSize_GB,sleepTime_s,r_wSpeedCutOff,accessToken)
 elif ('API' in sheetName):
     dbx = dropboxBatch.dropboxAPI(excelName,sheetName,accessToken,chunkSize_MB)
 utils.combineLogs()
