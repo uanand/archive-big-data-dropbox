455ec9e1200052b5621e2ce861dc573ed394f446
Utkarsh Anand <utkarsh.aanand@gmail.com>
2021-05-27 12:46:37 +0800

    Update default parameters.

--- app.py
+++ app.py
@@ -11,18 +11,18 @@ fileSizeLimit_GB = 100
 chunkSizeSplit_MB = 1024
 
 # PARAMETERS FOR DATA UPLOAD USING APP
-dropboxDir = r'E:\Dropbox'   # DIRECTORY WHERE DROPBOX SYNC DIRECTORY IS LOCATED
-batchSize_GB = 200
-sleepTime_s = 120
-r_wSpeedCutOff = 0.2
-accessToken = '*************' # GET DROPBOX ACCESS TOKEN BY CREATING AN APP HERE - https://www.dropbox.com/developers/apps
+dropboxDir = r'E:\Dropbox (NUSCentreofBioImagin)'   # DIRECTORY WHERE DROPBOX SYNC DIRECTORY IS LOCATED
+batchSize_GB = 500
+sleepTime_min = 30
+batchTimeLimit_hour=12
+accessToken = '#############################' # GET DROPBOX ACCESS TOKEN BY CREATING AN APP HERE - https://www.dropbox.com/developers/apps
 
 # PARAMETERS FOR DATA UPLOAD USING API
 chunkSize_MB = 128 # in MB
 
 dp = dataPrep.dataPrep(excelName,sheetName,fileSizeLimit_GB,chunkSizeSplit_MB)
 if ('APP' in sheetName):
-    dbx = dropboxBatch.dropboxApp(excelName,sheetName,dropboxDir,accessToken,batchSize_GB,sleepTime_s,r_wSpeedCutOff)
+    dbx = dropboxBatch.dropboxApp(excelName,sheetName,dropboxDir,accessToken,batchSize_GB,sleepTime_min,batchTimeLimit_hour)
 elif ('API' in sheetName):
     dbx = dropboxBatch.dropboxAPI(excelName,sheetName,accessToken,chunkSize_MB)
 utils.combineLogs()
