7ecde433a46006cd993917dee20f06b2d0b34c7f
Utkarsh Anand <utkarsh.aanand@gmail.com>
2021-07-10 10:48:36 +0800

    Compress large Gatan movies into a a single zip file.

--- preProcess/1_listDirToZip.py
+++ preProcess/1_listDirToZip.py
@@ -1,6 +1,9 @@
 import numpy
 import pandas
 import os
+import re
+import platform
+import time
 
 fileSizeLimit_GB = 500
 df = pandas.read_excel('zipDir.xlsx',sheet_name='listDirToZip',names=['inputDir'],usecols=[0])
@@ -57,7 +60,7 @@ for i in range(row):
         outFile.write('%s\t%d\t%d\t%.6f\n' %(parentDir,numSubDir,parentNumFiles,parentFolderSize))
 f.close()
 outFile.close()
-# ############################################################
+############################################################
 
 
 ############################################################
@@ -74,22 +77,91 @@ while (i<row):
     parentNumSubDir = df['numSubDir'][i]
     parentNumFiles = df['numFiles'][i]
     parentFolderSize = df['size'][i]
-    for j in range(i+1,row):
-        daughterDir = df['dirName'][j]
-        if (parentDir in daughterDir):
-            pass
+    if (i<row-1):
+        j = i+1
+        while (j<row):
+        # for j in range(i+1,row):
+            daughterDir = df['dirName'][j]
+            if (parentDir in daughterDir):
+                j += 1
+            else:
+                break
+        i = j
+        outFile.write('%s\t%d\t%d\t%.6f\n' %(parentDir,parentNumSubDir,parentNumFiles,parentFolderSize))
+    else:
+        outFile.write('%s\t%d\t%d\t%.6f\n' %(parentDir,parentNumSubDir,parentNumFiles,parentFolderSize))
+        i += 1
+outFile.close()
+
+###### GATAN MOVIES WILL BE COMPRESSED TO A SINGLE ZIP FILE EVEN IF LARGER THAN 500GB
+df = pandas.read_csv('directoryZipList.txt',delimiter='\t',names=['dirName','numSubDir','numFiles','size'],header=0)
+[row,col] = df.shape
+
+outFile = open('directoryZipList_Update.txt','w')
+outFile.write('Directory\tNumber of subdirectories\tNumber of files\tSize (GB)\n')
+
+i = 0
+while (i<row):
+    zipDir = df['dirName'][i]
+    zipNumSubDir = df['numSubDir'][i]
+    zipNumFiles = df['numFiles'][i]
+    zipFolderSize = df['size'][i]
+    parentDir = None
+    
+    if (platform.system()=='Linux'):
+        match1 = re.search('/Hour_\d\d/Minute_\d\d/Second_\d\d',zipDir)
+        match2 = re.search('/Hour_\d\d/Minute_\d\d',zipDir)
+        match3 = re.search('/Hour_\d\d',zipDir)
+        if (match1):
+            parentDir = zipDir.split(match1.group())[0]
+        elif (match2):
+            parentDir = zipDir.split(match2.group())[0]
+        elif (match3):
+            parentDir = zipDir.split(match3.group())[0]
+    elif (platform.system()=='Windows'):
+        match1 = re.search('\\\\Hour_\d\d\\\\Minute_\d\d\\\\Second_\d\d',zipDir)
+        match2 = re.search('\\\\Hour_\d\d\\\\Minute_\d\d',zipDir)
+        match3 = re.search('\\\\Hour_\d\d',zipDir)
+        if (match1):
+            parentDir = zipDir.split(match1.group())[0]
+        elif (match2):
+            parentDir = zipDir.split(match2.group())[0]
+        elif (match3):
+            parentDir = zipDir.split(match3.group())[0]
+    parentNumSubDir = zipNumSubDir+1
+    parentNumFiles = zipNumFiles
+    parentFolderSize = zipFolderSize
+    
+    if (i<row-1):
+        if (parentDir):
+            for j in range(i+1,row):
+                daughterDir = df['dirName'][j]
+                daughterNumSubDir = df['numSubDir'][j]+1
+                daughterNumFiles = df['numFiles'][j]
+                daughterFolderSize = df['size'][j]
+                if (parentDir in daughterDir):
+                    parentNumSubDir += daughterNumSubDir
+                    parentNumFiles += daughterNumFiles
+                    parentFolderSize += daughterFolderSize
+                else:
+                    outFile.write('%s\t%d\t%d\t%.6f\n' %(parentDir,parentNumSubDir,parentNumFiles,parentFolderSize))
+                    i = j
+                    break
         else:
-            break
-    i = j
-    outFile.write('%s\t%d\t%d\t%.6f\n' %(parentDir,parentNumSubDir,parentNumFiles,parentFolderSize))
-    if (i==row-1):
-        break
+            outFile.write('%s\t%d\t%d\t%.6f\n' %(zipDir,zipNumSubDir,zipNumFiles,zipFolderSize))
+            i += 1
+    else:
+        outFile.write('%s\t%d\t%d\t%.6f\n' %(zipDir,zipNumSubDir,zipNumFiles,zipFolderSize))
+        i += 1
 outFile.close()
 ############################################################
 
 
 ############################################################
 # STEP 4 - DELETE EXCESS FILES
+time.sleep(5)
 os.remove('directoryList.txt')
 os.remove('directoryStructure.txt')
+os.remove('directoryZipList.txt')
+os.rename('directoryZipList_Update.txt','directoryZipList.txt')
 ############################################################
